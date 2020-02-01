63688867c705f78fa6bfc8ebad55619b42908849
Chris Milner <christophermilner@btinternet.com>
2020-02-01 23:47:23 +0000

    Reduced test runtime

--- src/main/java/com/chrisdmilner/progressbar/examples/ExampleUtils.java
+++ src/main/java/com/chrisdmilner/progressbar/examples/ExampleUtils.java
@@ -1,9 +1,15 @@
 package com.chrisdmilner.progressbar.examples;
 
 class ExampleUtils {
+    private static double pauseMultiplier = 1;
+
+    static void setPauseMultiplier(double pauseMultiplier) {
+        ExampleUtils.pauseMultiplier = pauseMultiplier;
+    }
+
     static void pause(double seconds) {
         try {
-            Thread.sleep(Math.round(seconds * 1000));
+            Thread.sleep(Math.round(seconds * 1000 * pauseMultiplier));
         } catch (InterruptedException e) {
             System.err.println("Thread interrupted while paused");
             e.printStackTrace();
--- src/test/java/com/chrisdmilner/progressbar/examples/ExampleTests.java
+++ src/test/java/com/chrisdmilner/progressbar/examples/ExampleTests.java
@@ -1,14 +1,19 @@
-package com.chrisdmilner.progressbar;
+package com.chrisdmilner.progressbar.examples;
 
-import com.chrisdmilner.progressbar.examples.*;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-class NormalUsageTests {
+class ExampleTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        ExampleUtils.setPauseMultiplier(0.0);
+    }
 
     @ParameterizedTest
     @MethodSource("provideExamples")
